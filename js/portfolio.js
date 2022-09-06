341e8d1dffe7b51368621b2744ba1ff84b3e4773
recker3015 <homsweethome252@gmail.com>
2022-09-07 02:20:56 +0530

    Add files via upload
    updates for mobile view

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,32 @@
+let slideIndex = 1;
+showSlides(slideIndex);
 
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 //intersection observer 
-
 var obitem=document.querySelectorAll('.obitem');
 for(let i=0;i<obitem.length;i++){
 var io=new IntersectionObserver(function(entries){
@@ -12,7 +38,7 @@ var io=new IntersectionObserver(function(entries){
                     targets: entry.target,
                     opacity: [0,1],
                     easing: 'easeOutSine',
-                    duration: 800,
+                    duration: 500,
                     translateY: ['100%','0%'],
                     autoplay: false,
                 }
